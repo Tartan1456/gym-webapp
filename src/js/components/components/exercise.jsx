d7ab228bf16ed808286284bb2c826223ab93e321
Malcolm McConaghy <malcolm.mcconaghy@onthebeach.co.uk>
2019-03-03 16:37:13 +0000

    Make workout days based on objects on Json

--- src/js/components/components/exercise.jsx
+++ src/js/components/components/exercise.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Exercise = ({ exerciseName, sets, weight }) => (
+const Exercise = ({ exerciseName, sets, reps, weight }) => (
   <div className='workout__exercise'>
     <div className='workout__exercise-name'>
       { exerciseName }
     </div>
     <div className='workout__sets'>
-      { sets }
+      { sets + 'x' + reps }
     </div>
     <div className='workout__weight'>
-      { weight }
+      { weight + 'kg' }
     </div>
   </div>
 );
--- src/js/components/containers/workout.jsx
+++ src/js/components/containers/workout.jsx
@@ -4,39 +4,31 @@ import { Link } from 'react-router-dom'
 import Workout from '../components/workout';
 import Exercise from '../components/exercise';
 
-let workoutDays = require('../../../js/json.json');
+let workoutDays = require('../../../js/workoutDays.json');
 
-console.log(workoutDays);
 
 const WorkoutContainer = () => {
-  workoutDays.forEach((workoutDay) => {
-    return <div>workoutDay.date</div>
-  });
-
   return (
     <div className='workouts-wrapper'>
-      <Link to='workoutDay'>
-        <Workout
-          date='Fri 31 Aug'
-          muscleSet='Legs &amp; Shoulders'
-        >
-          <Exercise
-            exerciseName='SQ'
-            sets='5x5'
-            weight='30kg'
-          />
-        </Workout>
-      </Link>
-      <Workout
-        date='Mon 3 Sep'
-        muscleSet='Back'
-      >
-        <Exercise
-          exerciseName='DL'
-          sets='5x5'
-          weight='60kg'
-        />
-      </Workout>
+      {workoutDays.map((workoutDay, i) => {
+        return (
+          <Link key={ i } to='workoutDay'>
+            <Workout
+              key={ i }
+              { ...workoutDay }
+            >
+              {workoutDay.exercises.map((exercise, i) => {
+                return (
+                  <Exercise
+                    key={ i }
+                    { ...exercise }
+                  />
+                );
+              })}
+            </Workout>
+          </Link>
+        )
+      })}
     </div>
   )
 };
